11284f53a97a4fa3362a661d505ce613de1c6b4c
b3ar1sh <hkelleher1@icloud.com>
2022-06-24 19:22:29 -0400

    yes

--- js/app.js
+++ js/app.js
@@ -131,3 +131,22 @@ async function fetchWithRetry(url)  {
     return fetch_retry(url);
   });
 }
+
+const gallery = document.getElementById('gallery');
+const popup = document.getElementById('popup');
+const selectedImage = document.getElementById('selectedImage');
+const imageIndexes = [1,2,3,4,5,6,7,8,9,10];
+const selectedIndex = null;
+
+imageIndexes.forEach((i) => {
+  const image = document.createElement('img');
+  image.src = `/images/gallery/${i}.png`;
+  image.alt = `sBoards NFT`;
+  image.classList.add('galleryImg')
+
+  image.addEventListener('click', () =>{
+    //popup stuff
+  })
+
+  gallery.appendChild(image);
+})
